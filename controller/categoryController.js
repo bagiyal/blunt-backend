e3b51e7f1cbcdb01c485e24055eb3b44c30e6d2b
Abhishek Bagiyal <63339447+bagiyal@users.noreply.github.com>
2024-07-24 15:53:33 +0530

    changes in addCategory function

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -25,22 +25,48 @@ const getCategories = async (req, res) => {
 };
 
 const addCategory = async (req, res) => {
-  const isExists = await user.findOne({
-    where: {
-      phoneNumber: req.body.phoneNumber,
-    },
-  });
-  console.log("--created category",isExists);
-  if (isExists) {
-    isExists.categories = req.body.categories;
-    await isExists.save();
-    return res.json({
-      status: true,
-      message: "category update succesfully",
-      userData: isExists
+  try {
+    const { phoneNumber, email, categories } = req.body;
+
+    // Build the query conditionally based on the presence of phoneNumber or email
+    let query = {};
+    if (phoneNumber) {
+      query.phoneNumber = phoneNumber;
+    } else if (email) {
+      query.email = email;
+    } else {
+      return res.status(400).json({
+        status: false,
+        message: "Either phoneNumber or email is required",
+      });
+    }
+
+    const isExists = await user.findOne({ where: query });
+    console.log("--created category", isExists);
+
+    if (isExists) {
+      isExists.categories = categories;
+      await isExists.save();
+      return res.json({
+        status: true,
+        message: "Category updated successfully",
+        userData: isExists,
+      });
+    } else {
+      return res.status(404).json({
+        status: false,
+        message: "User not found",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      status: false,
+      message: "Internal server error",
     });
   }
 };
+
 
 const getCategoriesById = async (req, res) => {
   try {
